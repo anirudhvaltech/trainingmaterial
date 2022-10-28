aac5bd7f521eb13fc61bddf8be0de0aa1f485e32
Anirudh <113085155+avh-17@users.noreply.github.com>
2022-10-28 18:10:16 +0530

    Add files via upload

---
+++ day32/components/users.vue
@@ -0,0 +1,28 @@
+<template>  
+    <div>
+      <pre>{{ JSON.stringify(users,null,1) }}</pre>
+      <button @click="getUsers">Get Data</button>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+    export default {
+     name : "AppUsers",
+      data(){
+        return {
+          users : []
+        }
+      },
+      methods : {
+       getUsers(){
+          axios.get("https://reqres.in/api/users?page=2")
+          .then( res => this.users = res.data.data )
+          .catch( err => console.log( err ))
+      }
+    }
+}
+</script>
+  
+<style>
+</style>
